05e5c8a3fc1c6d8c9a4c936c8947d21fb2b60153
AbhirupGhosh92 <joshmcgrath123@gmail.com>
2021-01-12 20:38:10 +0000

    Added binary seearch tree

--- trees/BinarySearchTree.py
+++ trees/BinarySearchTree.py
@@ -1,3 +1,5 @@
+#Implementinng a binary seqarch tree. For sake of visualisation we use a. dictionary to visualize
+
 class BinarySearchTree:
     
     class __Node__:
@@ -16,5 +18,45 @@ class BinarySearchTree:
         if(self.__root__ == None):
             self.__root__ = self.__Node__(value)
         else:
-            if(value < self.__root__.value):
-                
+           current = self.__root__
+           while(True):
+                if(value < current.value):
+                    if(current.left == None):
+                       current.left = self.__Node__(value)
+                       break
+                    else:
+                        current = current.left
+                else:
+                    if(current.right == None):
+                       current.right = self.__Node__(value)
+                       break
+                    else:
+                        current = current.right
+
+    def lookup(self,value):
+        if(self.__root__ == None):
+            return False
+        else:
+            current = self.__root__
+            while(current != None):
+                if(value < current.value):
+                    current = current.left
+                elif(value > current.value):
+                    current = current.right
+                elif(value == current.value):
+                    return current
+            return False
+
+
+
+
+tree = BinarySearchTree()
+tree.insert(9)
+tree.insert(4)
+tree.insert(1)
+tree.insert(6)
+tree.insert(20)
+tree.insert(15)
+tree.insert(70)
+
+print(tree.lookup(90))
