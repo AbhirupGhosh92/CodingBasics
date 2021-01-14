cdb419c5a2a67dfd5e8a588724b1b992124c8d6c
AbhirupGhosh92 <joshmcgrath123@gmail.com>
2021-01-14 21:03:08 +0000

    added sorting

---
+++ sorting/Sorting.py
@@ -0,0 +1,143 @@
+arr = [1,3,2,1,5,3,4,7,5,9,6,9,0,3,1,7,2,5,2,7,2,7,9,34,6,8]
+
+def bubble_sort(arr):
+    for i in range(0,len(arr)):
+        for j in range(0,len(arr)):
+            if(arr[i] < arr[j]):
+                temp = arr[j]
+                arr[j] = arr[i]
+                arr[i] = temp
+
+
+bubble_sort(arr)
+print("Bubble sort -->",arr)
+
+def selection_sort(arr):
+    for i in range(0,len(arr)):
+        min = i
+        temp = arr[i]
+        for j in range(i,len(arr)):
+            if(arr[j] < arr[min]):
+                min = j
+                arr[j] = arr[min]
+                arr[min] = temp
+
+bubble_sort(arr)
+print("Selection sort -->",arr)
+
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)): 
+        key = arr[i] 
+        j = i-1
+        while j >=0 and key < arr[j] : 
+                arr[j+1] = arr[j] 
+                j -= 1
+        arr[j+1] = key
+
+insertion_sort(arr)
+print("Insertion sort -->",arr)
+
+def merge(arr, l, m, r): 
+    n1 = m - l + 1
+    n2 = r- m 
+  
+    # create temp arrays 
+    L = [0] * (n1) 
+    R = [0] * (n2) 
+  
+    # Copy data to temp arrays L[] and R[] 
+    for i in range(0 , n1): 
+        L[i] = arr[l + i] 
+  
+    for j in range(0 , n2): 
+        R[j] = arr[m + 1 + j] 
+  
+    # Merge the temp arrays back into arr[l..r] 
+    i = 0     # Initial index of first subarray 
+    j = 0     # Initial index of second subarray 
+    k = l     # Initial index of merged subarray 
+  
+    while i < n1 and j < n2 : 
+        if L[i] <= R[j]: 
+            arr[k] = L[i] 
+            i += 1
+        else: 
+            arr[k] = R[j] 
+            j += 1
+        k += 1
+  
+    # Copy the remaining elements of L[], if there 
+    # are any 
+    while i < n1: 
+        arr[k] = L[i] 
+        i += 1
+        k += 1
+  
+    # Copy the remaining elements of R[], if there 
+    # are any 
+    while j < n2: 
+        arr[k] = R[j] 
+        j += 1
+        k += 1
+  
+# l is for left index and r is right index of the 
+# sub-array of arr to be sorted
+def mergeSort(arr,l,r): 
+    if l < r: 
+  
+        # Same as (l+r)//2, but avoids overflow for 
+        # large l and h 
+        m = (l+(r-1))//2
+  
+        # Sort first and second halves 
+        mergeSort(arr, l, m) 
+        mergeSort(arr, m+1, r) 
+        merge(arr, l, m, r)  
+  
+mergeSort(arr,0,len(arr)-1)
+print("Merge sort -->",arr)
+
+def partition(arr, low, high): 
+    i = (low-1)         # index of smaller element 
+    pivot = arr[high]     # pivot 
+  
+    for j in range(low, high): 
+  
+        # If current element is smaller than or 
+        # equal to pivot 
+        if arr[j] <= pivot: 
+  
+            # increment index of smaller element 
+            i = i+1
+            arr[i], arr[j] = arr[j], arr[i] 
+  
+    arr[i+1], arr[high] = arr[high], arr[i+1] 
+    return (i+1) 
+  
+# The main function that implements QuickSort 
+# arr[] --> Array to be sorted, 
+# low  --> Starting index, 
+# high  --> Ending index 
+  
+# Function to do Quick sort 
+  
+  
+def quickSort(arr, low, high): 
+    if len(arr) == 1: 
+        return arr 
+    if low < high: 
+  
+        # pi is partitioning index, arr[p] is now 
+        # at right place 
+        pi = partition(arr, low, high) 
+  
+        # Separately sort elements before 
+        # partition and after partition 
+        quickSort(arr, low, pi-1) 
+        quickSort(arr, pi+1, high) 
+  
+  
+quickSort(arr, 0, len(arr)-1)
+print("Quick sort -->",arr)
+  
